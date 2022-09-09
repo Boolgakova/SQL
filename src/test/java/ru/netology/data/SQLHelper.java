4622d483c5eaf9934c358b58ad4d8e81f84542cd
Boolgakova <boolgakova@gmail.com>
2022-09-09 12:38:41 +0300

    Tests added

--- src/test/java/ru/netology/data/SQLHelper.java
+++ src/test/java/ru/netology/data/SQLHelper.java
@@ -21,7 +21,7 @@ public class SQLHelper {
 
     @SneakyThrows
     public static DataHelper.VerificationCode getVerificationCode(){
-        var codeSQL = "SELECT code FROM auth_codes ORDERED BY created DESC LIMIT 1";
+        var codeSQL = "SELECT code FROM auth_codes ORDER BY created DESC LIMIT 1";
         try (var conn = getConn()) {
             var result = runner.query(conn, codeSQL, new ScalarHandler<String>());
             return new DataHelper.VerificationCode(result);
--- src/test/java/ru/netology/test/BankLoginTest.java
+++ src/test/java/ru/netology/test/BankLoginTest.java
@@ -10,20 +10,69 @@ import static ru.netology.data.SQLHelper.cleanDatabase;
 import static com.codeborne.selenide.Selenide.open;
 
 public class BankLoginTest {
-
     @AfterAll
-    static void tearDown(){
+    static void tearDown() {
         cleanDatabase();
     }
 
     @Test
-    void shouldSuccessfulLogin(){
+    void shouldSuccessfulLogin() {
         var loginPage = open("http://localhost:9999", LoginPage.class);
         var authInfo = DataHelper.getAuthInfoWithTestData();
         var verificationPage = loginPage.validLogin(authInfo);
         verificationPage.verifyVerificationPageVisibility();
         var verificationCode = SQLHelper.getVerificationCode();
-        verificationPage.verify(verificationCode.getCode());
+        verificationPage.validVerify(verificationCode.getCode());
     }
 
+    @Test
+    void shouldShowErrorWhenRandomUser() {
+        var loginPage = open("http://localhost:9999", LoginPage.class);
+        var authInfo = DataHelper.getRandomUser();
+        loginPage.validLogin(authInfo);
+        loginPage.getError();
+    }
+
+    @Test
+    void shouldShowErrorWhenInvalidLogin() {
+        var loginPage = open("http://localhost:9999", LoginPage.class);
+        var authInfo = new DataHelper.AuthInfo(DataHelper.getRandomUser().getLogin(), DataHelper.getAuthInfoWithTestData().getPassword());
+        loginPage.validLogin(authInfo);
+        loginPage.getError();
+    }
+
+    @Test
+    void shouldShowErrorWhenInvalidPassword() {
+        var loginPage = open("http://localhost:9999", LoginPage.class);
+        var authInfo = new DataHelper.AuthInfo(DataHelper.getAuthInfoWithTestData().getLogin(), DataHelper.getRandomUser().getPassword());
+        loginPage.validLogin(authInfo);
+        loginPage.getError();
+    }
+
+    @Test
+    void shouldBlockWhenInvalidPasswordThreeTimes() {
+        var loginPage = open("http://localhost:9999", LoginPage.class);
+        var authInfo = new DataHelper.AuthInfo(DataHelper.getAuthInfoWithTestData().getLogin(), DataHelper.getRandomUser().getPassword());
+        loginPage.validLogin(authInfo);
+        loginPage.getError();
+        loginPage.cleanFields();
+        var authInfo1 = new DataHelper.AuthInfo(DataHelper.getAuthInfoWithTestData().getLogin(), DataHelper.getRandomUser().getPassword());
+        loginPage.validLogin(authInfo1);
+        loginPage.getError();
+        loginPage.cleanFields();
+        var authInfo2 = new DataHelper.AuthInfo(DataHelper.getAuthInfoWithTestData().getLogin(), DataHelper.getRandomUser().getPassword());
+        loginPage.validLogin(authInfo2);
+        loginPage.getBlockError();
+    }
+
+    @Test
+    void shouldShowErrorWhenRandomVerificationCode() {
+        var loginPage = open("http://localhost:9999", LoginPage.class);
+        var authInfo = DataHelper.getAuthInfoWithTestData();
+        var verificationPage = loginPage.validLogin(authInfo);
+        verificationPage.verifyVerificationPageVisibility();
+        var verificationCode = DataHelper.getRandomVerificationCode().getCode();
+        verificationPage.validVerify(verificationCode);
+        verificationPage.getError();
+    }
 }
