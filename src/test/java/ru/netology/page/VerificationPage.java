6d913c054383ccd5818ddd255656ec0119b6a25b
Boolgakova <boolgakova@gmail.com>
2022-09-14 13:47:47 +0300

    Error locator in VerificationPage fixed

--- src/test/java/ru/netology/page/VerificationPage.java
+++ src/test/java/ru/netology/page/VerificationPage.java
@@ -3,20 +3,23 @@ package ru.netology.page;
 import com.codeborne.selenide.SelenideElement;
 import ru.netology.data.DataHelper;
 
+import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selenide.$;
 
 public class VerificationPage {
     private SelenideElement codeField = $("[data-test-id=code] input");
     private SelenideElement verifyButton = $("[data-test-id=action-verify]");
-    private SelenideElement error = $("data-test-id=error-notification");
+    private SelenideElement error = $(".notification__content");
 
     public void verifyVerificationPageVisibility() {
         codeField.shouldBe(visible);
     }
 
     public void getError() {
-        error.shouldBe(visible);
+        error
+                .shouldBe(visible)
+                .shouldHave(text("Неверно указан код! Попробуйте ещё раз."));
     }
 
     public void verify(String verificationCode) {
